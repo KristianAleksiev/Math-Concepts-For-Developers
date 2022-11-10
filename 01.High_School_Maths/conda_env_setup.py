1714acf8efa3ad0af4e91174edfddc5728cd8f4b
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-11-10 23:52:34 +0200

    Add HS maths

---
+++ 01.High_School_Maths/conda_env_setup.py
@@ -0,0 +1,12 @@
+"""
+1. Conda virtual env
+conda env list
+conda activate env_name
+conda deactivate
+conda list - package manager
+conda install anaconda - installing default anaconda packages
+
+2. Equations in python
+sympy - symbolic computations
+numpy - numeric computations
+"""
---
+++ 01.High_School_Maths/hs_maths_recap.py
@@ -0,0 +1,25 @@
+"""
+1. Divide and conquer method
+- Useful for any kid of problem
+Assumption - Complicated things are a combination of many simple things
+
+
+2. Scientific approach method
+- Define the question
+- Do research
+- Build a hypothesis - Need to be able to be proved or disproved (to be valid)
+- Test the hypothesis, build an experiment (Controlled environment) - If it works =>
+ Analyze the data else fix the experiment
+- Results align with hypo => Success, if they don't, define new question or new hypo
+- Report the results, start again from the top
+
+Why scientific method
+- Useful when something new is examined
+- Common logic
+- Use of experiments
+
+3. Mathematical notations
+
+
+
+"""
