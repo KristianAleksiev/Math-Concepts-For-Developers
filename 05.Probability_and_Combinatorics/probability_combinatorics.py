4b9b9b703ffb27044f8eda4775f8373d5fccf2de
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-11-16 22:58:39 +0200

    Add probability and combinatorics

---
+++ 05.Probability_and_Combinatorics/probability_combinatorics.py
@@ -0,0 +1,51 @@
+"""
+1. Probability
+- Experiment - initial conditions - > outcome
+- Random event - A set of outcomes of an experiment which happens with a certain probability
+- Random variable - the value outcome of the experiment
+- It is not possible to predict the next outcome of a random event, but we can perform trials(the more the better)
+- Statistical definition of probability -> lim m/n , n->infinity
+- The sum of all probabilities = 1 (100% of event happening)
+
+2. Combinatorics
+- Counting objects and group of objects
+- Finite (countable) number of outcomes
+- All outcomes have equal probability
+- Counting rules - Computing a combinatorial probability, desired outcomes
+
+- Rule of sum => One of all
+- Rule of product => One after the other
+
+- Permutation - Pn -> n! - shuffling
+- Variations - Permutation of subset -> n! / (n-k)!
+- Combinations - n! / (n-k)! * k! - n choose k
+
+3. Algebra of events
+- Event space - the set (omega) of all possible events
+- Logical relations are the same as set operations:
+AND - intersection
+OR - union
+NOT - complement
+
+4. Conditional probability
+- Additional information about the experiment outcome can change the probabilities
+
+5. Applications
+6. Statistical distributions
+- Frequency distributions (histograms)
+- Discrete distributions (n count events, which are known) - Probability mass function
+- Continuous distribution - Probability density function
+
+Common distributions:
+- Bernoulli and Uniform distribution
+- Binomial distribution (one coin 2 times, two coins 1 time)
+- Normal distribution
+7. Central limit theorem
+- The sum of many independent random variables tends to a normal distribution even if the original random variables
+are not normally distributed OR
+- The sampling distribution of the mean of any independent random variable will be normal or nearly normal if the
+sample is large enough (e.g. 30-40 +)
+e.g. - Customers in a shop
+
+
+"""
