817a1ac04dd568b1e62ae98ba717eca2c2481c4d
Kristian Aleksiev <103513324+KristianAleksiev@users.noreply.github.com>
2022-11-17 21:29:50 +0200

    Add Statistics

---
+++ 06.Statistics/statistics_notes.py
@@ -0,0 +1,64 @@
+"""
+1. Descriptive and inferential statistics
+Descriptive
+- Summarize and describe data
+- No predictions, describe what is seen
+- Statistical population - all items of interest
+
+Inferential
+- In many cases the population is too large
+- Population is represented by a subset (sample)
+- The population characteristics can be estimated by the sample
+- Random sampling e.g. voting predictions, mean salary by age
+
+2. Population and sample
+
+Sampling:
+- Define the population
+- Sampling frame - a set of items from the population
+- Sampling method - rules of selection from the frame
+- Sample size
+
+Sampling methods:
+- Random sampling
+- Stratified sampling - categories
+
+3. Properties of statistical distribution
+
+Summarizing distributions: ( summary statistics )
+- Histogram - complete description of the sample distribution
+- Central tendency - Do the values tend to cluster around a center?
+- Modes - How many clusters are there and where are they?
+- Variance - How spread out is the distribution?
+- Tails - How quickly do probabilities change as we move away from the center(s)?
+- Outliers - Are there extreme values, far from center?
+
+Arithmetic Mean - The sum of all measurements divided by the number of observations
+Median - The middle value of the distro, sorted, less sensitive to outliers
+Mode - The most frequent item, >1, multimodal distribution
+
+Standard deviation (Variance)
+- The distance between a point and the mean
+
+Skewness (Асиметрия) +ve zero -ve - Посока на опашката
+- Moment of distributions
+
+Kurtosis (Ексцес)  <3, =3, >3, form of distribution, density in center vs density in skew, Гаусово разпределение(3)
+
+4. Visualizing data
+
+5. Covariance and correlation
+Measure of the joint variability of two variables:
+- Positive - One increases, the other increases
+- Negative - One increases, the other decreases
+- Zero - The two variables don't vary at all
+
+- Covariance matrix, main diagonal
+6. Common misconceptions
+- Correlation vs causation
+
+- Reverse causation - The faster the windmill rotates, the more wind there is => Windmill causes wind
+- Lurking variable - The more firefighters there are to put out a fire, the greater the damage => FFs cause damage
+- Bidirectional relationship - Predators affect prey, prey food, prey food affects predators
+- Coincidence
+"""
